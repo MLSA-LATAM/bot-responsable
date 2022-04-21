f3b3e2b575414a3a9162f125b2b2ac525653099c
Felipe Flores <felipe_1357@outlook.com>
2022-04-21 09:31:10 -0500

    Add files via upload

---
+++ src/AnimalData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalHelp
+{
+    public class AnimalData
+    {
+        protected Animal[] Animals { get; set; }
+
+        public AnimalData(string fn)
+        {
+            var data = System.IO.File.ReadAllLines(fn);
+            Animals = (from z in data.Skip(1)
+                         select new Animal(z)).ToArray();
+        }
+
+        public string GetAnimal(string animal)
+        {
+            return Animals.FirstOrDefault(
+                c => c.Spanish.ToLower() == animal.ToLower())?.English;
+        }
+    }
+
+    public class Animal{
+        public Animal(string s)
+        {
+            var t = s.Split(',');
+            English = t[0].Trim('"');
+            Spanish = t[1].Trim('"');
+        }
+        public string English { get; set; }
+        public string Spanish { get; set; }
+    }
+}
---
+++ src/EchoBot.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.15.2
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnimalHelp.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var spanish = turnContext.Activity.Text;
+            var f = System.IO.Path.Combine(System.Environment.CurrentDirectory, @"animals.csv");
+            var cd = new AnimalData(f);
+            var english = cd.GetAnimal(spanish);
+            var replyText = english == null
+                ? "No conozco ese animal"
+                : $"{spanish} se dice {english} en inglés";
+            await turnContext.SendActivityAsync(replyText); ;
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hola. Soy un bot de asistencia para aprender los animales en inglés. Si ingresas el nombre de un animal, te puedo decir cómo se dice en inglés.";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
